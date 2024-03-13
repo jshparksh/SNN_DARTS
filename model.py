fix gradient function

model.py
@@ -187,7 +187,7 @@ class NetworkCIFAR(nn.Module):
     self.global_pooling = nn.AdaptiveAvgPool2d(1)
     self.classifier = nn.Linear(C_prev, num_classes)
     self.identity_for_spike = nn.Identity()
-    self.pact_log = PACT_with_log_quantize(time_step=16)
+    #self.pact_log = PACT_with_log_quantize(time_step=16)
 
   def forward(self, input):
     E_add = 0
@@ -204,7 +204,7 @@ class NetworkCIFAR(nn.Module):
       E_neuron += cell_e_neuron
     self._total_spike_energy = E_add + E_neuron
     out = self.global_pooling(s1)
-    out = self.pact_log(out)
+    #out = self.pact_log(out)
     logits = self.classifier(out.view(out.size(0),-1))
     logits = self.identity_for_spike(logits)
     

operations.py
@@ -56,7 +56,7 @@ class AvgPool(nn.Module):
     def spike_datas(self):
         self.spike_rate = [non_zero_ifm / num_ifm for non_zero_ifm, num_ifm in zip(self.non_zero_ifm, self.num_ifm)]
         # quan_info = [ofm, base]
-        self.time_neuron = [torch.round(torch.where(quan_info[0] == 0, torch.tensor(0, dtype=torch.float32).cuda(), -torch.log(quan_info[0]))/torch.log(torch.tensor(quan_info[1]))) for quan_info in self.quan_infos]
+        self.time_neuron = [torch.round(torch.where(quan_info[0] == 0, torch.tensor(0, dtype=torch.float32).cuda(), -torch.log(quan_info[0]))/torch.log(quan_info[1])) for quan_info in self.quan_infos]
         return [self.flops, self.spike_rate, self.time_neuron]
 
 class MaxPool(nn.Module):
@@ -77,7 +77,7 @@ class MaxPool(nn.Module):
         return output
 
     def spike_datas(self):
-        self.time_neuron = [torch.round(torch.where(quan_info[0] == 0, torch.tensor(0, dtype=torch.float32).cuda(), -torch.log(quan_info[0]))/torch.log(torch.tensor(quan_info[1]))) for quan_info in self.quan_infos]
+        self.time_neuron = [torch.round(torch.where(quan_info[0] == 0, torch.tensor(0, dtype=torch.float32).cuda(), -torch.log(quan_info[0]))/torch.log(quan_info[1])) for quan_info in self.quan_infos]
         return [[0], [torch.tensor(0).cuda()], self.time_neuron]
     
 class Conv(nn.Module):
@@ -107,7 +107,7 @@ class Conv(nn.Module):
     
     def spike_datas(self):
         self.spike_rate = [non_zero_ifm / num_ifm for non_zero_ifm, num_ifm in zip(self.non_zero_ifm, self.num_ifm)]
-        self.time_neuron = [torch.round(torch.where(quan_info[0] == 0, torch.tensor(0, dtype=torch.float32).cuda(), -torch.log(quan_info[0]))/torch.log(torch.tensor(quan_info[1]))) for quan_info in self.quan_infos]
+        self.time_neuron = [torch.round(torch.where(quan_info[0] == 0, torch.tensor(0, dtype=torch.float32).cuda(), -torch.log(quan_info[0]))/torch.log(quan_info[1])) for quan_info in self.quan_infos]
         return [self.flops, self.spike_rate, self.time_neuron]
     
 class DilConv(nn.Module):
@@ -141,7 +141,7 @@ class DilConv(nn.Module):
 
     def spike_datas(self):
         self.spike_rate = [non_zero_ifm / num_ifm for non_zero_ifm, num_ifm in zip(self.non_zero_ifm, self.num_ifm)]
-        self.time_neuron = [torch.round(torch.where(quan_info[0] == 0, torch.tensor(0, dtype=torch.float32).cuda(), -torch.log(quan_info[0]))/torch.log(torch.tensor(quan_info[1]))) for quan_info in self.quan_infos]
+        self.time_neuron = [torch.round(torch.where(quan_info[0] == 0, torch.tensor(0, dtype=torch.float32).cuda(), -torch.log(quan_info[0]))/torch.log(quan_info[1])) for quan_info in self.quan_infos]
         return [self.flops, self.spike_rate, self.time_neuron]
     
 class SepConv(nn.Module):
@@ -182,7 +182,7 @@ class SepConv(nn.Module):
 
     def spike_datas(self):
         self.spike_rate = [non_zero_ifm / num_ifm for non_zero_ifm, num_ifm in zip(self.non_zero_ifm, self.num_ifm)]
-        self.time_neuron = [torch.round(torch.where(quan_info[0] == 0, torch.tensor(0, dtype=torch.float32).cuda(), -torch.log(quan_info[0]))/torch.log(torch.tensor(quan_info[1]))) for quan_info in self.quan_infos]
+        self.time_neuron = [torch.round(torch.where(quan_info[0] == 0, torch.tensor(0, dtype=torch.float32).cuda(), -torch.log(quan_info[0]))/torch.log(quan_info[1])) for quan_info in self.quan_infos]
         return [self.flops, self.spike_rate, self.time_neuron]
     
 class Identity(nn.Module):
@@ -200,7 +200,7 @@ class Identity(nn.Module):
         return output
     
     def spike_datas(self):
-        self.time_neuron = [torch.round(torch.where(quan_info[0] == 0, torch.tensor(0, dtype=torch.float32).cuda(), -torch.log(quan_info[0]))/torch.log(torch.tensor(quan_info[1]))) for quan_info in self.quan_infos]
+        self.time_neuron = [torch.round(torch.where(quan_info[0] == 0, torch.tensor(0, dtype=torch.float32).cuda(), -torch.log(quan_info[0]))/torch.log(quan_info[1])) for quan_info in self.quan_infos]
         # assume input is output of spike neuron -> no need to calculate
         return [[0], [torch.tensor(0).cuda()], self.time_neuron]
         
@@ -257,5 +257,5 @@ class FactorizedReduce(nn.Module):
 
     def spike_datas(self):
         self.spike_rate = [non_zero_ifm / num_ifm for non_zero_ifm, num_ifm in zip(self.non_zero_ifm, self.num_ifm)]
-        self.time_neuron = [torch.round(torch.where(quan_info[0] == 0, torch.tensor(0, dtype=torch.float32).cuda(), -torch.log(quan_info[0]))/torch.log(torch.tensor(quan_info[1]))) for quan_info in self.quan_infos]
+        self.time_neuron = [torch.round(torch.where(quan_info[0] == 0, torch.tensor(0, dtype=torch.float32).cuda(), -torch.log(quan_info[0]))/torch.log(quan_info[1])) for quan_info in self.quan_infos]
         return [self.flops, self.spike_rate, self.time_neuron]

quantization.py
@@ -60,11 +60,11 @@ class log_quantize(InplaceFunction):
         min_act = scale*(base**(-ctx.constant)+base**(1-ctx.constant))/2
         ltm      = x < min_act
         gi       = (~ltm).float()
-        grad_x   = grad_output*gi
+        grad_x   = grad_output #*gi
         #grad_base = torch.sum(torch.where(x<min_act, grad_output.float(), grad_output*x*(1-base)/(math.sqrt(base)*(1+base)**2))).view(-1) # torch.tensor(0.,).cuda()
-        grad_base = torch.sum(torch.where(x<min_act, grad_output*(x>0).float(), grad_output*x*(1-base)/(math.sqrt(base)*(1+base)**2))).view(-1) # torch.tensor(0.,).cuda()
+        #grad_base = torch.sum(torch.where(x<min_act, grad_output*(x>0).float(), grad_output*x*(1-base)/(math.sqrt(base)*(1+base)**2))).view(-1) # torch.tensor(0.,).cuda()
         #grad_base = torch.sum(grad_output*x.ge(min_act)*x*(1-base)/(math.sqrt(base)*(1+base)**2).float()).view(-1)
-        #grad_base = torch.sum(torch.where(x<min_act, grad_output, grad_output*x*(1-base)/(math.sqrt(base)*(1+base)**2))).view(-1) # torch.tensor(0.,).cuda()
+        grad_base = torch.sum(torch.where(x<min_act, grad_output*((x>0).float()), 1/2*grad_output/math.sqrt(base))).view(-1) #grad_output*x*(1-base)/(math.sqrt(base)*(1+base)**2))).view(-1) # torch.tensor(0.,).cuda()
         return grad_x, None, grad_base, None
     
 class PACT_log_quantize(InplaceFunction):
@@ -75,7 +75,8 @@ class PACT_log_quantize(InplaceFunction):
         y = torch.clamp(x, min=0., max=alpha.item())
         normed_ofm = (y / alpha)
         log_value = torch.where(normed_ofm==0.0, torch.tensor(-time_step).float().cuda(), torch.log(normed_ofm)/torch.log(torch.tensor(base)))
-        round = torch.where((log_value <= 0.0), torch.round(log_value), torch.tensor(0.).cuda())
+        round = torch.round(log_value)
+        #round = torch.where((log_value <= 0.0), torch.round(log_value), torch.tensor(0.).cuda())
         y = torch.where(round > -time_step, base**round, torch.tensor(0.,).cuda()) * alpha
         return y, normed_ofm
     
@@ -85,23 +86,25 @@ class PACT_log_quantize(InplaceFunction):
         min_act = alpha*(base**(-ctx.constant)+base**(1-ctx.constant))/2
         
         lt0      = x < 0
-        gta      = x > alpha
         ltm      = x < min_act
-        gi       = (~(ltm|gta)).float()
+        gta      = x > alpha
+        gi       = (~(lt0|gta)).float()
         grad_x   = grad_output*gi
         grad_alpha = torch.sum(grad_output*x.ge(alpha).float()).view(-1)
-        grad_base = torch.sum(torch.where(x<min_act, grad_output*(x>0).float(), grad_output*x*(1-base)/(math.sqrt(base)*(1+base)**2))).view(-1) # torch.tensor(0.,).cuda()
+        grad_base = torch.sum(torch.where(x<min_act, grad_output*((x>0).float()), grad_output*x*(1-base)/(math.sqrt(base)*(1+base)**2))).view(-1) # torch.tensor(0.,).cuda()
         return grad_x, grad_alpha, grad_base, None
     
 class PACT(nn.Module):
     def __init__(self, alpha=5.):
         super(PACT, self).__init__()
-        self.alpha = nn.Parameter(torch.Tensor([alpha]), requires_grad=True)
+        self.alpha = nn.Parameter(torch.Tensor([alpha]), requires_grad=False)
         self.relu = nn.ReLU(inplace=False)
     def forward(self, input):
         qinput = pact_function.apply(input, self.alpha, 0, -3)
         return qinput
 
+
+"""
 class PACT_with_log_quantize(nn.Module):
     def __init__(self, alpha=5., base=2., time_step=16):
         super(PACT_with_log_quantize, self).__init__()
@@ -114,11 +117,13 @@ class PACT_with_log_quantize(nn.Module):
         self.normed_ofm = normed_output
         return qinput
 """    
+
+
 class PACT_with_log_quantize(nn.Module):
     def __init__(self, alpha=5., base=2., time_step=16):
         super(PACT_with_log_quantize, self).__init__()
-        self.alpha = nn.Parameter(torch.Tensor([alpha]), requires_grad=True)
-        self.base = nn.Parameter(torch.Tensor([base]), requires_grad=True)
+        self.alpha = nn.Parameter(torch.Tensor([alpha]), requires_grad=False)
+        self.base = nn.Parameter(torch.Tensor([base]), requires_grad=False)
         self.time_step = time_step
         
     def forward(self, input):
@@ -126,4 +131,3 @@ class PACT_with_log_quantize(nn.Module):
         self.normed_ofm = (qinput / self.alpha)
         qinput = log_quantize.apply(qinput, self.alpha, self.base, self.time_step)
         return qinput
-"""